57658647eafb734cf4573d0ce0969c54843cfe39
Ashish-develper1010 <ac6100861@gmail.com>
2025-06-17 17:32:50 +0530

    Time: 31 ms (57.47%), Space: 51.9 MB (39.41%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,46 +1,31 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList();
-    public List<List<Integer>> threeSum(int[] arr) {
-        Arrays.sort(arr);
-        for(int i=0;i<arr.length;i++)
-        {
-            if(i==0 || arr[i-1]!=arr[i])
-            {
-                twoSumSorted(i+1,arr.length-1,0-arr[i],arr);
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> threesum = new ArrayList<>();
+        if(nums.length < 3) return new ArrayList<>();
+        Arrays.sort(nums);
+        // Set<List<Integer>> result = new HashSet<>();
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = nums.length-1;
+            int target = -nums[i];
+
+            while(j < k) {
+                
+                int sum = nums[j] + nums[k];
+                if(sum == target) {
+                    threesum.add(Arrays.asList(nums[i] , nums[j] , nums[k]));
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j < k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+                }
+                else if(sum < target) j++;
+                else k--;
             }
         }
-        return res;
-    }
-    
-
-    void twoSumSorted(int i,int j,int target,int[] arr)
-    {
-        int a = arr[i-1];
-        while(i<j)
-        {
-            if(arr[i]+arr[j]>target)
-            {
-                j--;
-            }
-            else if(arr[i]+arr[j]<target)
-            {
-                i++;
-            }
-            else
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(a);
-                list.add(arr[i]);
-                list.add(arr[j]);
-
-                res.add(list);
-
-                while(i<j && arr[i]==arr[i+1]) i++;
-                while(i<j && arr[j]==arr[j-1]) j--;
-
-                i++;
-                j--;
-            }
-        }
+        return threesum;
+        
     }
 }
