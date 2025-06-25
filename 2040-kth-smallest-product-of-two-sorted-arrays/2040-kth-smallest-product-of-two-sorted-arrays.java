cde6f04cb64c35e3f87b5fd839802d8ddf921e12
Ashish-develper1010 <ac6100861@gmail.com>
2025-06-25 22:30:45 +0530

    Time: 659 ms (70.15%), Space: 55.4 MB (66.67%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,63 +1,47 @@
 class Solution {
-
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        int n1 = nums1.length, n2 = nums2.length;
-        int pos1 = 0, pos2 = 0;
-        while (pos1 < n1 && nums1[pos1] < 0) {
-            pos1++;
-        }
-        while (pos2 < n2 && nums2[pos2] < 0) {
-            pos2++;
-        }
-        long left = (long) -1e10, right = (long) 1e10;
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long count = 0;
-            int i1 = 0, i2 = pos2 - 1;
-            while (i1 < pos1 && i2 >= 0) {
-                if ((long) nums1[i1] * nums2[i2] > mid) {
-                    i1++;
-                } else {
-                    count += pos1 - i1;
-                    i2--;
-                }
-            }
-            i1 = pos1;
-            i2 = n2 - 1;
-            while (i1 < n1 && i2 >= pos2) {
-                if ((long) nums1[i1] * nums2[i2] > mid) {
-                    i2--;
-                } else {
-                    count += i2 - pos2 + 1;
-                    i1++;
-                }
-            }
-            i1 = 0;
-            i2 = pos2;
-            while (i1 < pos1 && i2 < n2) {
-                if ((long) nums1[i1] * nums2[i2] > mid) {
-                    i2++;
-                } else {
-                    count += n2 - i2;
-                    i1++;
-                }
-            }
-            i1 = pos1;
-            i2 = 0;
-            while (i1 < n1 && i2 < pos2) {
-                if ((long) nums1[i1] * nums2[i2] > mid) {
-                    i1++;
-                } else {
-                    count += n1 - i1;
-                    i2++;
-                }
-            }
-            if (count < k) {
-                left = mid + 1;
+        long low = -10000000000L, high = 10000000000L;
+
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+            if (countLessEqual(nums1, nums2, mid) < k) {
+                low = mid + 1;
             } else {
-                right = mid - 1;
+                high = mid;
             }
         }
-        return left;
+
+        return low;
+    }
+
+    private long countLessEqual(int[] A, int[] B, long x) {
+        long count = 0;
+
+        for (int a : A) {
+            if (a == 0) {
+                if (x >= 0) count += B.length;
+                // else 0 * b > x → contributes nothing
+            } else if (a > 0) {
+                // want b ≤ x / a
+                int left = 0, right = B.length;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if ((long)a * B[mid] <= x) left = mid + 1;
+                    else right = mid;
+                }
+                count += left;
+            } else { // a < 0
+                // want b ≥ ceil(x / a)
+                int left = 0, right = B.length;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if ((long)a * B[mid] <= x) right = mid;
+                    else left = mid + 1;
+                }
+                count += B.length - left;
+            }
+        }
+
+        return count;
     }
 }
