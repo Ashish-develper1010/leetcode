e15f2098a7e0a30fa9f9b6a4c597a977a91fa912
Ashish-develper1010 <ac6100861@gmail.com>
2025-06-27 04:06:03 +0530

    Time: 1 ms (100%), Space: 42.6 MB (39.12%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,60 +1,52 @@
-class pair {
-    int r;
-    int c;
-    int t;
-
-    public pair(int r, int c, int t) {
-        this.r = r;
-        this.c = c;
-        this.t = t;
-    }
-}
-
 class Solution {
+    class pair {
+        int r;
+        int c;
+        int t;
+
+        public pair(int r , int c, int t) {
+            this.t = t;
+            this.r = r;
+            this.c = c;
+        }
+    }
     public int orangesRotting(int[][] grid) {
+        Queue<pair> q = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
+        int freshOrange = 0;
 
-        Queue<pair> q = new LinkedList<>();
-        int freshCount = 0;
-
-        // Step 1: Put all initially rotten oranges into the queue
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.add(new pair(i, j, 0));
-                } else if (grid[i][j] == 1) {
-                    freshCount++;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.add(new pair(i , j , 0));
                 }
+                else if(grid[i][j] == 1) freshOrange++;
             }
         }
 
-        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
         int maxTime = 0;
+        int[][] direction = {{0,1} , {1,0}, {0,-1},{-1,0}};
 
-        // Step 2: BFS traversal
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             pair p = q.poll();
             int r = p.r;
             int c = p.c;
             int t = p.t;
 
-            maxTime = Math.max(maxTime, t);
+            maxTime = Math.max(maxTime , t);
 
-            for (int[] d : dirs) {
-                int nr = r + d[0];
-                int nc = c + d[1];
+            for(int[] dir : direction) {
+                int row = r + dir[0];
+                int col = c + dir[1];
 
-                // Check bounds and if fresh orange
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1) {
-                    grid[nr][nc] = 2;  // Make it rotten
-                    freshCount--;      // Decrease fresh count
-                    q.add(new pair(nr, nc, t + 1));  // Add to queue with incremented time
+                if(row < n && row >= 0 && col < m && col >= 0 && grid[row][col] == 1) {
+                    grid[row][col] = 2;
+                    freshOrange--;
+                    q.add(new pair(row , col , t+1));
                 }
             }
         }
-
-        // Step 3: If any fresh orange left, return -1
-        return (freshCount == 0) ? maxTime : -1;
+        return freshOrange == 0 ? maxTime : -1;
     }
 }
