add406ee04ed4e1d1f449045d0248928f42aa99a
Ashish-develper1010 <ac6100861@gmail.com>
2025-09-30 14:34:36 +0530

    Time: 9 ms (15.24%), Space: 44.8 MB (44.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,31 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int len = s.length();
-
-        if(len == 0) return 0;   // if length is 0 then no operation possible.
-
-        HashMap<Character , Integer> freqMap = new HashMap<>();
+        int maxLen = Integer.MIN_VALUE;
+        HashMap<Character , Integer> map = new HashMap<>();
 
         int start = 0 , end = 0;
-        int maxLen = Integer.MIN_VALUE;
 
-        while(end < len) {
-            char ch = s.charAt(end);
+        while(end < s.length()) {
+            map.put(s.charAt(end) , map.getOrDefault(s.charAt(end) , 0)+1);
 
-            freqMap.put(ch , freqMap.getOrDefault(ch , 0)+1);  // count freq of current window
-
-            while(freqMap.get(ch) >= 2) {
-                char c = s.charAt(start);
-                freqMap.put(c , freqMap.get(c)-1);   // manage freq to be unique.
-                if(freqMap.get(c) == 0) freqMap.remove(c);
-
+            while(map.get(s.charAt(end)) > 1) {
+                map.put(s.charAt(start) , map.get(s.charAt(start)) - 1);
+                if(map.get(s.charAt(start)) == 0) map.remove(s.charAt(start));
                 start++;
             }
-
-            maxLen = Math.max(maxLen , end - start + 1);   // manage count of length of unique subarray.
+            maxLen = Math.max(maxLen , end - start + 1);
             end++;
         }
-
-        return maxLen == Integer.MIN_VALUE ? 0 : maxLen;  // idea to return smartly.
+        return maxLen == Integer.MIN_VALUE ? 0 : maxLen;
     }
 }
