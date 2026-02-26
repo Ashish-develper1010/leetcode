127562cf72671671e42ee4b3f3f6635faafb9d21
Ashish-develper1010 <ac6100861@gmail.com>
2026-02-26 12:11:44 +0530

    Time: 2 ms (40.12%), Space: 45.7 MB (17.11%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,19 +1,49 @@
-import java.math.BigInteger;
-
 class Solution {
-    public int numSteps(String s) {
-        BigInteger val = new BigInteger(s, 2);
-        int count = 0;
 
-        while (!val.equals(BigInteger.ONE)) {
-            if (val.testBit(0)) { // odd number check
-                val = val.add(BigInteger.ONE);
-            } else {
-                val = val.shiftRight(1); // divide by 2
-            }
-            count++;
+    public int numSteps(String s) {
+        return solve(s, 0);
+    }
+
+    private int solve(String s, int count) {
+
+        // Base case
+        if (s.equals("1")) {
+            return count;
         }
 
-        return count;
+        int len = s.length();
+
+        // If even → last bit = 0
+        if (s.charAt(len - 1) == '0') {
+
+            // divide by 2 → remove last bit
+            return solve(s.substring(0, len - 1), count + 1);
+
+        } else {
+
+            // odd → add 1
+            String added = addOne(s);
+            return solve(added, count + 1);
+        }
+    }
+
+    // binary addition helper
+    private String addOne(String s) {
+
+        StringBuilder sb = new StringBuilder(s);
+        int i = sb.length() - 1;
+
+        while (i >= 0 && sb.charAt(i) == '1') {
+            sb.setCharAt(i, '0');
+            i--;
+        }
+
+        if (i >= 0) {
+            sb.setCharAt(i, '1');
+        } else {
+            sb.insert(0, '1');
+        }
+
+        return sb.toString();
     }
 }
