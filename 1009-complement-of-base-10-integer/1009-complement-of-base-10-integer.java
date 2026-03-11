bd1109de8b3a60b62b83e61bf3576f7413ec4ac6
Ashish-develper1010 <ac6100861@gmail.com>
2026-03-11 12:14:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,17 +1,16 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        String binary = Integer.toBinaryString(n);
-        String flipedBinary = flip(binary);
-        int res = Integer.parseInt(flipedBinary,2);
-        return res;
-    }
+        int ans = 0, pow = 1;
 
-    private String flip(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()) {
-            if(ch == '0') sb.append('1');
-            else sb.append('0');
+        if(n == 0) return 1;
+
+        while(n > 0) {
+            int digit = 1 - (n % 2);
+            ans += digit * pow;
+            pow *= 2;
+            n /= 2;
         }
-        return sb.toString();
+
+        return ans;
     }
 }
