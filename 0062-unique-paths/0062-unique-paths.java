6201a6ad0e13c021ab8d62f0f6246b6ecf4e3552
Ashish-develper1010 <ac6100861@gmail.com>
2026-05-03 09:45:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,39 @@
 class Solution {
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp = new int[m+1][n+1];
+    //     for(int[] d : dp) {
+    //         Arrays.fill(d , -1);
+    //     }
+    //     return helper(0 , 0 , m , n, dp);
+    // }
+    // public int helper(int i , int j , int m , int n, int[][] dp) {
+    //     if(i >= m || j >= n) return 0;
+    //     if(i == m-1 && j == n-1) return 1;
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int rightWays = helper(i , j + 1, m , n, dp);
+    //     int leftWays = helper(i + 1, j , m , n, dp);
+
+    //     return dp[i][j] = rightWays + leftWays;
+    // }
+
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        for(int[] d : dp) {
-            Arrays.fill(d , -1);
+        int[][] dp = new int[m][n];
+
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
-        return helper(0 , 0 , m , n, dp);
-    }
-    public int helper(int i , int j , int m , int n, int[][] dp) {
-        if(i >= m || j >= n) return 0;
-        if(i == m-1 && j == n-1) return 1;
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
 
-        if(dp[i][j] != -1) return dp[i][j];
-        int rightWays = helper(i , j + 1, m , n, dp);
-        int leftWays = helper(i + 1, j , m , n, dp);
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
 
-        return dp[i][j] = rightWays + leftWays;
+        return dp[m-1][n-1];
+
     }
 }
