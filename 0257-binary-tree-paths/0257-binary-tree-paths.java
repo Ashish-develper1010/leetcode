a4f68d476b9b0fd8512a61eefd4277f31fc6e816
Ashish-develper1010 <ac6100861@gmail.com>
2026-05-04 21:44:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,28 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        dfs(root, "", result);
-        return result;
-    }
+    public void dfs(TreeNode root, String str, List<String> res) {
+        if(root == null) return;
 
-    private void dfs(TreeNode root, String path, List<String> result) {
-        if (root == null) return;
-
-        // Add current node to path
-        if (path.length() == 0) {
-            path += root.val;
-        } else {
-            path += "->" + root.val;
+        if(str.length() == 0) {
+            str += root.val;
+        }
+        else {
+            str += ("->" + root.val);
         }
 
-        // If leaf node → add to result
-        if (root.left == null && root.right == null) {
-            result.add(path);
+        if(root.left == null && root.right == null) {
+            res.add(str);
             return;
         }
 
-        // Recurse left and right
-        dfs(root.left, path, result);
-        dfs(root.right, path, result);
+        dfs(root.left , str, res);
+        dfs(root.right , str, res);
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+
+        dfs(root , "", res);
+
+        return res;
     }
 }
