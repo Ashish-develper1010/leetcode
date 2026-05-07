8cf4b24286f83293f7cc45aa3a2e90f63f9e778f
Ashish-develper1010 <ac6100861@gmail.com>
2026-05-07 17:24:29 +0530

    Time: 140 ms (44.91%), Space: 286.4 MB (29.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,29 +1,39 @@
 class Solution {
-    public boolean hasPath(ArrayList<ArrayList<Integer>> graph , int src , int dest , boolean[] vis) {
-        if(src == dest) {
-            return true;
-        }
-
-        vis[src] = true;
-
-        for(int i : graph.get(src)) {
-            if(!vis[i] && hasPath(graph , i , dest , vis))
-                return true;
-        }
-        return false;
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
 
         for(int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
 
-        for(int[] e : edges) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
+        for(int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        return hasPath(graph , source , destination , new boolean[n]);
+
+        return bfs(adj, source, destination);
+    }
+
+    private boolean bfs(List<List<Integer>> adj, int src, int dest) {
+        int n = adj.size();
+        Queue<Integer> qu = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+        qu.offer(src);
+        vis[src] = true;
+
+        while(!qu.isEmpty()) {
+            int curr = qu.poll();
+            for(int ele : adj.get(curr)) {
+                if(!vis[ele]) {
+                    qu.offer(ele);
+                    vis[ele] = true;
+                }
+            }
+        }
+
+        return vis[dest];
     }
 }
