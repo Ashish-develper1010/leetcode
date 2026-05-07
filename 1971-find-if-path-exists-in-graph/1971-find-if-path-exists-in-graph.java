76056127aaaf6b652ce5dce57691fbf7586396a4
Ashish-develper1010 <ac6100861@gmail.com>
2026-05-07 17:25:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
 
+        // Build graph
         for(int i = 0; i < edges.length; i++) {
+
             int u = edges[i][0];
             int v = edges[i][1];
 
@@ -18,22 +21,28 @@ class Solution {
     }
 
     private boolean bfs(List<List<Integer>> adj, int src, int dest) {
-        int n = adj.size();
+
         Queue<Integer> qu = new LinkedList<>();
-        boolean[] vis = new boolean[n];
+        boolean[] vis = new boolean[adj.size()];
+
         qu.offer(src);
         vis[src] = true;
 
         while(!qu.isEmpty()) {
+
             int curr = qu.poll();
-            for(int ele : adj.get(curr)) {
-                if(!vis[ele]) {
-                    qu.offer(ele);
-                    vis[ele] = true;
+
+            if(curr == dest) return true;
+
+            for(int nei : adj.get(curr)) {
+
+                if(!vis[nei]) {
+                    vis[nei] = true;
+                    qu.offer(nei);
                 }
             }
         }
 
-        return vis[dest];
+        return false;
     }
 }
