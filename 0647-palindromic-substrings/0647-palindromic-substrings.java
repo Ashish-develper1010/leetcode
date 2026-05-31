399a3253885b058a27d996e62de13b9d9634283b
Ashish-develper1010 <ac6100861@gmail.com>
2026-05-31 11:08:43 +0530

    Time: 28 ms (22.93%), Space: 61.2 MB (5.31%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,21 +3,25 @@ class Solution {
         int len = s.length();
         int count = 0;
 
+        Boolean[][] dp = new Boolean[len][len];
+
         for(int i = 0; i < len; i++) {
             for(int j = i; j < len; j++) {
-                if(isPalindrome(s, i, j)) count++;
+                if(isPalindrome(s, i, j, dp)) count++;
             }
         }
 
         return count;
     }
 
-    public boolean isPalindrome(String str, int i, int j) {
+    public boolean isPalindrome(String str, int i, int j, Boolean[][] dp) {
         if( i > j) return true;
         if(i == j) return true;
 
-        if(str.charAt(i) != str.charAt(j)) return false;
+        if(dp[i][j] != null) return dp[i][j];
 
-        else return isPalindrome(str, i+1, j-1);
+        if(str.charAt(i) != str.charAt(j)) return dp[i][j] = false;
+
+        else return dp[i][j] = isPalindrome(str, i+1, j-1, dp);
     }
 }
