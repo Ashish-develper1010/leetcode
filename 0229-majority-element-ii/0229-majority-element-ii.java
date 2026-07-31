8162e8502050171ce260d84b0fb1c40d6ded959a
Ashish-develper1010 <ac6100861@gmail.com>
2026-07-31 16:39:32 +0530

    Time: 2 ms (99.95%), Space: 52.4 MB (66.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,40 +1,37 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) return result;
+        if(nums == null || nums.length == 0) return new ArrayList<>();
 
-        int num1 = -1, num2 = -1, count1 = 0, count2 = 0;
-        int n = nums.length;
+        int cand1 = 0, cand2 = 0;
+        int cnt1 = 0, cnt2 = 0;
 
-        for (int num : nums) {
-            if (num == num1) {
-                count1++;
-            } else if (num == num2) {
-                count2++;
-            } else if (count1 == 0) {
-                num1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                num2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        for(int num : nums) {
+            if(num == cand1) cnt1++;
+            else if(num == cand2) cnt2++;
+            else if(cnt1 == 0) {
+                cand1 = num;
+                cnt1++;
+            }
+            else if(cnt2 == 0) {
+                cand2 = num;
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
             }
         }
 
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == num1) count1++;
-            else if (num == num2) count2++;
+        cnt1 = cnt2 = 0;
+
+        for(int num : nums) {
+            if(num == cand1) cnt1++;
+            else if(num == cand2) cnt2++;
         }
 
-        if (count1 > n / 3) result.add(num1);
-        if (count2 > n / 3) result.add(num2);
-
-        return result;
+        ArrayList<Integer> list = new ArrayList<>();
+        if(cnt1 > nums.length / 3) list.add(cand1);
+        if(cnt2 > nums.length / 3) list.add(cand2);
+        return list;
     }
 }
